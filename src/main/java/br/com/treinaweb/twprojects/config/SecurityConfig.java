e585d7b56954cc72f2677d4e232b9814466eb91d
Cleyson <cleysonph@gmail.com>
2024-02-05 13:28:39 -0300

    04:07 - Configurando o Login

--- src/main/java/br/com/treinaweb/twprojects/config/SecurityConfig.java
+++ src/main/java/br/com/treinaweb/twprojects/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,9 +15,20 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
             .authorizeHttpRequests(customizer -> customizer
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(customizer -> customizer
+                .loginPage("/auth/login")
+                .defaultSuccessUrl("/")
+                .permitAll()
             )
             .build();
+    }
+
+    @Bean
+    WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+            .requestMatchers("/webjars/**");
     }
     
 }
